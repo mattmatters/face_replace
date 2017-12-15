5041fef8be899b8cdd4a6477285a5fcfa46778d2
Matt Lewis <domattthings@gmail.com>
2017-12-15 18:43:21 -0500

    Typo fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='face_replace',
-    version='0.1.8',
+    version='0.1.9',
     description='Simple facial recognition and face replacement',
     author='Matt Lewis',
     author_email='domattthings@gmail.com',
     license='MIT',
     url='https://github.com/mattmatters/face_replace',
-    download_url='https://github.com/mattmatters/face_replace/archive/0.1.8.tar.gz',
+    download_url='https://github.com/mattmatters/face_replace/archive/0.1.9.tar.gz',
     py_modules=['face_replace'],
     packages=['face_replace'],
     install_requires=[
