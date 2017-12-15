51de04abe4da2f4c7ecf3a8bea0ad007f073c08b
Matt Lewis <domattthings@gmail.com>
2017-12-15 18:56:17 -0500

    Include additional files in setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='face_replace',
-    version='0.1.9',
+    version='0.1.10',
     description='Simple facial recognition and face replacement',
     author='Matt Lewis',
     author_email='domattthings@gmail.com',
     license='MIT',
     url='https://github.com/mattmatters/face_replace',
-    download_url='https://github.com/mattmatters/face_replace/archive/0.1.9.tar.gz',
+    download_url='https://github.com/mattmatters/face_replace/archive/0.1.10.tar.gz',
     py_modules=['face_replace'],
     packages=['face_replace'],
     install_requires=[
@@ -16,4 +16,11 @@ setup(
         'opencv-python',
         'pillow',
     ],
+    include_package_data=True,
+    package_data={
+        'paths':[
+            'paths/haarcascade_frontaface_alt.xml',
+            'paths/haarcascade_profileface.xml'
+        ]
+    }
 )
