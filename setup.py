2b602520cdf29f729adcd6c3fcc4b6f9e04aa395
Matt Lewis <domattthings@gmail.com>
2017-12-14 19:34:20 -0500

    Adjust config

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='face_replace',
-    version='0.1.5',
+    version='0.1.6',
     description='Simple facial recognition and face replacement',
     author='Matt Lewis',
     author_email='domattthings@gmail.com',
     license='MIT',
     url='https://github.com/mattmatters/face_replace',
-    download_url='https://github.com/mattmatters/face_replace/archive/0.1.5.tar.gz',
+    download_url='https://github.com/mattmatters/face_replace/archive/0.1.6.tar.gz',
     py_modules=['face_replace'],
+    packages=['face_replace'],
     install_requires=[
         'numpy',
         'opencv-python',
